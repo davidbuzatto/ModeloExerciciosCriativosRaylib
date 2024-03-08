0f89fdb6856fd61bd9e07b02b4922f639dba053f
David Buzatto <davidbuzatto@gmail.com>
2024-03-07 22:00:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * @brief Modelo para desenvolvimento de exercícios criativos usando a engine
  * de jogos Raylib (https://www.raylib.com/).
  * 
- * @copyright Copyright (c) 2023
+ * @copyright Copyright (c) 2024
  */
 
 #include <stdio.h>
@@ -17,21 +17,22 @@ int main( void ) {
 
     /*-----------------------------------------------------
      * A lógica inicial do seu programa deve vir aqui:
-     *     - declaração de variáveis
-     *     - entrada de dados.
+     *     - declaração de variáveis;
+     *     - entrada de dados;
+     *     - processamentos adicionais.
      ----------------------------------------------------*/
     
     
-    // ativa suavização (antialiasing)
+    // ativa a suavização (antialiasing)
     SetConfigFlags( FLAG_MSAA_4X_HINT );
 
-    // cria uma janela de 400 pixels de largura por 200 de altura
+    // cria uma janela de 800 pixels de largura por 600 de altura
     InitWindow( 800, 600, "Título da Janela" );
 
     // configura a quantidade de quatros por segundo da engine
     SetTargetFPS( 60 );    
 
-    // enquanto nãp é sinalizado que a janela deve ser fechada
+    // enquanto não é sinalizado que a janela deve ser fechada
     while ( !WindowShouldClose() ) {
 
         // inicia o processo de desenho
@@ -41,14 +42,13 @@ int main( void ) {
         ClearBackground( WHITE );
 
         /*----------------------------------------------------------------------
-         * A lógica do seu desenho deve vir aqui, assim como o
-         * processamento adicional de valores etc.
+         * A lógica do seu desenho deve vir aqui.
          ---------------------------------------------------------------------*/
 
 
 
         /*----------------------------------------------------------------------
-         * A lógica do seu desenho deve terminar na linha acima
+         * A lógica do seu desenho deve terminar na linha acima.
          ---------------------------------------------------------------------*/
 
         // termina o desenho
