b8504cd606c41ee52b34cb9391fd3f9fc5f9881b
David Buzatto <davidbuzatto@gmail.com>
2025-04-16 09:17:47 -0300

    atualização para macos

---
+++ buildMacOS.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Custom build script (macOS shell script).
+#
+# Usage:
+#    ./build.sh: clean, compile and run
+#    ./build.sh -clean: clean compiled file
+#    ./build.sh -cleanAndCompile: clean compiled file and compile the project
+#    ./build.sh -compile: compile the project
+#    ./build.sh -compileAndRun: compile the project and run the compiled file
+#    ./build.sh -run: run the compiled file
+#
+# Author: Prof. Dr. David Buzatto
+
+CompiledFile=${PWD##*/}
+CompiledFile=${CompiledFile:-/}
+
+clean_project() {
+    echo "Cleaning..."
+    rm -f $CompiledFile
+}
+
+compile_project() {
+    echo "Compiling..."
+    cc main.c -o $CompiledFile \
+        -O1 \
+        -Wall \
+        -Wextra \
+        -Wno-unused-parameter \
+        -pedantic-errors \
+        -std=c99 \
+        -Wno-missing-braces \
+        -I src/include/ \
+        -L lib \
+        -lraylib \
+        -framework IOKit \
+        -framework Cocoa \
+        -framework OpenGL 
+}
+
+run_project() {
+    echo "Running..."
+    if [ -e $CompiledFile ]; then
+        ./$CompiledFile
+    else
+        echo "$CompiledFile does not exists!"
+    fi
+}
+
+if [ $# -eq 0 ]; then
+    clean_project
+    compile_project
+    run_project
+fi
+
+if [ "$1" = "-clean" ]; then
+    clean_project
+fi
+
+if [ "$1" = "-cleanAndCompile" ]; then
+    clean_project
+    compile_project
+fi
+
+if [ "$1" = "-compile" ]; then
+    compile_project
+fi
+
+if [ "$1" = "-compileAndRun" ]; then
+    compile_project
+    run_project
+fi
+
+if [ "$1" = "-run" ]; then
+    run_project
+fi
